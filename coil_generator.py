c957bdb475c0e419d3172c150a2e4d9afdb261c1
Teyber <rpteyber@gmail.com>
2020-10-19 09:55:20 -0700

    backup

--- coil_generator.py
+++ coil_generator.py
@@ -16,9 +16,9 @@ def main():
 #INPUT PARAMETERS
     width = 20 #mm
     height = 40 #mm
-    line_width = 1 #mm
-    line_spacing = 1
-    n_turns = 5
+    line_width = 0.2 #mm
+    line_spacing = 0.2
+    n_turns = 20
     coil_params = [width, height, line_width, line_spacing, n_turns]
 
 
@@ -27,15 +27,15 @@ def main():
         return
 
 #Pad parameters
-    winding_direction = 1 # don't change  
-    drill_diameter = 1 #args.drill_ratio * coil_params.line_width
-    pad_OD = drill_diameter + 0.5
+    winding_direction = 1 # +1 or -1   
+    drill_diameter = 0.6 #args.drill_ratio * coil_params.line_width
+    pad_OD = drill_diameter + 0.2
 
 
 
 #CREATE COIL
     segments, start_point, end_point = rectangle_coil_lines(coil_params, winding_direction)
-    pads = coil_pads(start_point, end_point, pad_OD, drill_diameter, 'CONNECT', 'RECTANGLE')
+    pads = coil_pads(start_point, end_point, pad_OD, drill_diameter, 'CONNECT')
 
     fp = coil_footprint('Coil', 'One-layer coil', 'coil', segments + pads)
     save_footprint(fp, 'test_footprint.kicad_mod')
@@ -60,32 +60,33 @@ def rectangle_coil_lines(coil_params, direction):
 
     # points.append((params.r_outer, - params.r_outer))
     for i in np.arange(n_turns):
+
         x = width/2 - i * (line_width + line_spacing)
         y = height/2 - i * (line_width + line_spacing)
 
-        next_x = x - (line_width + line_spacing)
-
-        if direction > 0:
+        if direction == 1: #counter-clockwise
+            next_y = y - (line_width + line_spacing)
+            turn_points = [
+                (x, y),
+                (x, -y),
+                (-x, -y),
+                (-x, next_y),
+            ]
+        elif direction  == -1: #clockwise
+            next_x = x - (line_width + line_spacing)
             turn_points = [
                 (-x, -y),
                 (x, -y),
                 (x, y),
                 (-next_x, y),
             ]
-        else: print('NOT DONE YET')
-            # turn_points = [
-            #     (-x, -y),
-            #     (-x, y),
-            #     (x, y),
-            #     (x, -next_y),
-            # ]
+
         points.extend(turn_points)
 
 
     lines = []
-    for i in range(len(points) - 1):
-        lines.append(kmt.Line(start=points[i], end=points[i + 1],
-                              width=line_width, layer='F.Cu'))
+    for i in np.arange(len(points) - 1):
+        lines.append(kmt.Line(start=points[i], end=points[i + 1], width=line_width, layer='F.Cu'))
 
     start_point = points[0]
     end_point = points[-1]
@@ -111,33 +112,18 @@ def coil_footprint(name, description, tags, other_parts):
     return fp
 
 
-def coil_pads(start_point, end_point, line_width, drill, pad_type, pad_shape):
-    if pad_type == 'SMT':
-        pad_type = kmt.Pad.TYPE_THT
-        pad_layer = kmt.Pad.LAYERS_THT
-        drill_kw = dict(drill=drill)
-    elif pad_type == 'THT':
-        pad_type = kmt.Pad.TYPE_THT
-        pad_layer = kmt.Pad.LAYERS_THT
-        drill_kw = dict(drill=drill)
-    elif pad_type == 'CONNECT':
-        pad_type = kmt.Pad.TYPE_CONNECT
-        pad_layer = kmt.Pad.LAYERS_THT
-        drill_kw = dict()
-    else:
-        raise Exception('pad_type must be one of "SMT","THT" or "CONNECT"')
-    if pad_shape == 'RECTANGLE':
-        pad_shape = kmt.Pad.SHAPE_RECT
-    elif pad_shape == 'CIRCLE':
-        pad_shape = kmt.Pad.SHAPE_CIRCLE
-    else:
-        raise Exception('pad_type must be one of "RECTANGLE" or "CIRCLE"')
-    if line_width - drill < 0.25:
-        raise Exception('The width of the ring has to be greater than 0.25mm')
+def coil_pads(start_point, end_point, line_width, drill, pad_type):
+
+    pad_type = kmt.Pad.TYPE_THT
+    pad_layer = kmt.Pad.LAYERS_THT
+    drill_kw = dict(drill=drill)
+
+    pad_shape = kmt.Pad.SHAPE_CIRCLE
+    
+
     pads = []
     for i, pos in enumerate([start_point, end_point]):
-        pad = kmt.Pad(number=i + 1, type=pad_type, shape=pad_shape,
-                      at=pos, size=[line_width, line_width], layers=pad_layer, **drill_kw)
+        pad = kmt.Pad(number=i + 1, type=pad_type, shape=pad_shape, at=pos, size=[line_width, line_width], layers=pad_layer, **drill_kw)
         pads.append(pad)
     return pads
 
