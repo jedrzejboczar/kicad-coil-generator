b4b515069924e15a2f856d2d789ccc288e415b7f
Chris Thierauf <christopher.thierauf@tufts.edu>
2023-01-02 14:15:26 -0500

    allow generation by spacing, refactor to allow calling core functionality

--- square.py
+++ square.py
@@ -1,6 +1,3 @@
-import math
-from math import pi
-
 import KicadModTree as kmt
 
 
@@ -13,7 +10,14 @@ def delta_r(params):
     return delta_r
 
 
-def coil_lines(params, direction=+1):
+def coil_lines(params):
+    if params.n_turns is None:
+        return coil_lines_by_spacing(params)
+    else:
+        return coil_lines_by_turns(params)
+
+
+def coil_lines_by_turns(params):
     """
     Creates a square spiral coil.
 
@@ -24,15 +28,17 @@ def coil_lines(params, direction=+1):
     """
     n_turns = int(params.n_turns)
     if abs(n_turns - params.n_turns) > 0.01:
-        print('[WARNING] square coil can only have integer number of turns;'
-              ' reducing n_turns to %d' % n_turns)
+        print(
+            "[WARNING] square coil can only have integer number of turns;"
+            " reducing n_turns to %d" % n_turns
+        )
 
     points = []
-    points.append((params.r_outer, - params.r_outer))
+    points.append((params.r_outer, -params.r_outer))
     for i in range(n_turns):
         r = params.r_outer - i * delta_r(params)
         next_r = r - delta_r(params)
-        if direction > 0:
+        if params.direction > 0:
             turn_points = [
                 (-r, -r),
                 (-r, r),
@@ -50,13 +56,28 @@ def coil_lines(params, direction=+1):
 
     lines = []
     for i in range(len(points) - 1):
-        lines.append(kmt.Line(start=points[i], end=points[i + 1],
-                              width=params.line_width, layer='F.Cu'))
+        lines.append(
+            kmt.Line(
+                start=points[i],
+                end=points[i + 1],
+                width=params.line_width,
+                layer="F.Cu",
+            )
+        )
 
     start_point = points[0]
     end_point = points[-1]
 
     return lines, start_point, end_point
+
+
+def coil_lines_by_spacing(params):
+    fill_space = params.r_outer - params.r_inner
+    line_space = params.line_width + params.spacing
+    n_turns = int(fill_space / line_space)
+    params.n_turns = n_turns
+    return coil_lines_by_turns(params)
+
 
 def line_spacing(params):
     """
@@ -65,5 +86,7 @@ def line_spacing(params):
     be high enough to avoid electrical interference.
     Negative spacing is...too small.
     """
-    return delta_r(params) - params.line_width
-
+    if params.spacing is not None:
+        return params.spacing
+    else:
+        return delta_r(params) - params.line_width
